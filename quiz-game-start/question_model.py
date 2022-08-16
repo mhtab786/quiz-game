f764f2b9b5860df269c03945348dfe9a97c6d7e8
mahtab hussain <103880201+mhtab786@users.noreply.github.com>
2022-08-16 13:02:35 +0530

    Add files via upload

---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+# ques=Question("what's my name","anthony")
+#
+# print(ques.answer)
